12e95558885b98fee37cbc5f3797af1eaa106748
zoecruse <41089494+zoecruse@users.noreply.github.com>
2020-04-25 15:07:00 -0700

    Update final.hpp

--- final.hpp
+++ final.hpp
@@ -9,41 +9,62 @@ struct Node{
     Node* right;
 };
 
+struct LLNode 
+{
+    int key;
+    LLNode* next;
+};
+
+
+
+class LinkedList
+{
+  private:
+    LLNode *head;
+
+  public:
+    LinkedList(){
+    	head = nullptr;
+    }
+    void insert(LLNode *prev, int newKey);
+    LLNode* searchList(int key);
+    void printList();
+};
+
 class BST{
     private:
         Node* root;
-        Node* createNode(int data);
+        Node* createNode(int data);//done
         /** since root is a private member we need helper functions
          to access root for insertion, searching and printing.
          These helper functions is used for performing recursion **/
+        Node* addNodeHelper(Node*, int);//DONE
+        void printTreeHelper(Node*); //DONE
+        void print2DUtilHelper(Node *, int); //DONE
 
-        Node* addNodeHelper(Node*, int);
-        void printTreeHelper(Node*);
-        void print2DUtilHelper(Node *, int);
+        Node* searchKeyHelper(Node*, int); //done
 
-        Node* searchKeyHelper(Node*, int);
+        Node* getMinValueNode(Node*); //DONE
+        Node* getMaxValueNode(Node*); //DONE
 
-        Node* getMinValueNode(Node*);
-        Node* getMaxValueNode(Node*);
+        void destroyNode(Node *root); //DONE
 
-        void destroyNode(Node *root);
-
-        Node* deleteNode(Node*, int);
+        Node* deleteNode(Node*, int);//DONE
 
 
     public:
-        Node* getRoot();                // Returns the root of the tree;
-        void addNode(int);              // function to insert a node in the tree.
-        bool searchKey(int);            // function to search a data in the tree
-        void printTree();               //function to print the tree
-        BST();                          // default constructor
-        BST(int data);                  // parameterized constructor
-        ~BST();                         // destructor
+        Node* getRoot();                // Returns the root of the tree; DONE
+        void addNode(int);              // function to insert a node in the tree. DONE
+        bool searchKey(int);            // function to search a data in the tree DONE
+        void printTree();               //function to print the tree DONE
+        BST();                          // default constructor DONE
+        BST(int data);                  // parameterized constructor DONE
+        ~BST();                         // destructor DONE
         //after this idk if any of it is necessary? like fr
-        void removeRange(int, int);
-        void print2DUtil(int);
-        bool isValidBST();
-        bool valid(Node*, int, int);
+        void removeRange(int, int); //DONE but unnecessary 
+        void print2DUtil(int); //DONE
+        bool isValidBST(); //DOne
+        bool valid(Node*, int, int); //DONE
 
 };
 #endif
